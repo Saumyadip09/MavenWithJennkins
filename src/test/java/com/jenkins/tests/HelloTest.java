6742d0fa560c0b22278449130621e97c47776772
saumyadip09 <saumyadip09@gmail.com>
2019-03-29 07:07:55 +0530

    commiting maven

---
+++ src/test/java/com/jenkins/tests/HelloTest.java
@@ -0,0 +1,12 @@
+package com.jenkins.tests;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		
+		StringBuffer str = new StringBuffer("HelloWorld");
+		System.out.println(str+" reverse is "+str.reverse());
+
+	}
+
+}
